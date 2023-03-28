b84e6a20692e09733e4036e75275794d832bf730
DavidVegaSa <davidvgsm@gmail.com>
2023-03-28 12:53:34 +0200

    tienda190

--- datos.js
+++ datos.js
@@ -1,24 +1,23 @@
 class DataStorage {
-    
-    static set(key, value) {
-      localStorage.setItem(key, JSON.stringify(value));
-    }
-  
-    static get(key) {
-      const value = localStorage.getItem(key);
-      if (value) {
-        return JSON.parse(value);
-      }
-      return null;
-    }
-    static remove(key) {
-      localStorage.removeItem(key);
-    }
-    static clear() {
-      localStorage.clear();
-    }
-    static getNum(){
-      localStorage.length;
-    }
+
+  static set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
   }
-  
+
+  static get(key) {
+    const value = localStorage.getItem(key);
+    if (value) {
+      return JSON.parse(value);
+    }
+    return null;
+  }
+  static remove(key) {
+    localStorage.removeItem(key);
+  }
+  static clear() {
+    localStorage.clear();
+  }
+  static getNum() {
+    localStorage.length;
+  }
+}
